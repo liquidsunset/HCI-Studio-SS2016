d9dbf35ceea303c311603d808d497a76260227d6
Daniel Brand <daniel.brand@liquidsunset.org>
2016-05-24 22:56:47 +0200

    better angels for 5 elements, still not the best

--- Code/src/LeapFXConstant.java
+++ Code/src/LeapFXConstant.java
@@ -10,5 +10,5 @@ public class LeapFXConstant {
     public static final int DOUBLE_ROUND_DECIMAL = 2;
     public static final double[] angelForThreeElements = {180.0, 100.0, 80.0, 0.0};
     public static final double[] angelForFourElements = {180.0, 105.0, 95.0, 85.0, 0.0};
-    public static final double[] angelForFiveElements = {180.0, 115.0, 105.0, 95.0, 85.0, 0.0};
+    public static final double[] angelForFiveElements = {180.0, 115.0, 100.0, 85.0, 75.0, 0.0};
 }
